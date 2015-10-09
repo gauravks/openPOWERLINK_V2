07642600905008dc328a35f5959ff4d5fe469a8c
Gaurav Kumar Singh <gaurav@kalycito.com>
2015-10-09 11:45:17 +0530

    [TASK] Add kernel modules for Windows kernel solution
    Windows kernel solution contains following components:
    - openPOWERLINK edrv module using NDIS intermediate driver library
      to communicate with native NIC device driver.
    - openPOWERLINK hres timer module which uses NDIS timer objects
      for continuous and one shot timers required.
    - CAL modules which uses IOCTLs to communicate with the
      application library.
    - Circular buffer target module for Windows kernel.
    
    Change-Id: I92fd97f33bc024dcf516ce4427f9c79bfbada8f6
    Signed-off-by: Gaurav Kumar Singh <gaurav@kalycito.com>

--- stack/src/kernel/pdo/pdokcalmem-local.c
+++ stack/src/kernel/pdo/pdokcalmem-local.c
@@ -179,10 +179,54 @@ tOplkError pdokcal_freeMem(BYTE* pMem_p, size_t memSize_p)
     return kErrorOk;
 }
 
+//------------------------------------------------------------------------------
+/**
+\brief  Map PDO shared memory
+
+This routine maps the PDO memory allocated in the kernel layer of the openPOWERLINK
+stack. This allows user stack to access the PDO memory directly.
+
+\param  pKernelMem_p            Pointer to the shared memory segment in kernel space.
+\param  pUserMem_p              Pointer to the shared memory segment in user space.
+\param  pMemSize_p              Size of PDO memory.
+
+\return The function returns a tOplkError error code.
+
+\ingroup module_pdokcal
+*/
+//------------------------------------------------------------------------------
+tOplkError pdokcal_mapMem(UINT8** pKernelMem_p, UINT8** pUserMem_p, size_t* pMemSize_p)
+{
+    UNUSED_PARAMETER(pKernelMem_p);
+    UNUSED_PARAMETER(pUserMem_p);
+    UNUSED_PARAMETER(pMemSize_p);
+
+    return kErrorOk;
+}
+
+//------------------------------------------------------------------------------
+/**
+\brief  Unmap PDO shared memory
+
+Unmap the PDO memory shared with the user layer. The memory will be freed in
+pdokcal_freeMem().
+
+\param  pMem_p                  Pointer to the shared memory segment.
+\param  memSize_p               Size of PDO memory.
+
+\ingroup module_pdokcal
+*/
+//------------------------------------------------------------------------------
+void pdokcal_unMapMem(UINT8* pMem_p, size_t memSize_p)
+{
+    UNUSED_PARAMETER(pMem_p);
+    UNUSED_PARAMETER(memSize_p);
+}
+
 //============================================================================//
 //            P R I V A T E   F U N C T I O N S                               //
 //============================================================================//
 /// \name Private Functions
 /// \{
 
-///\}
+/// \}
